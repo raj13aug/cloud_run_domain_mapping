7f343911f6613c5aec07031a07bf85f9714f8d88
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-05-10 22:13:51 +0530

    dns

--- dns.tf
+++ dns.tf
@@ -3,25 +3,39 @@ data "google_dns_managed_zone" "env_dns_zone" {
 }
 
 
-resource "google_dns_record_set" "default" {
-  name         = "cloudrun.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+# resource "google_dns_record_set" "default" {
+#   name         = "cloudrun.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+#   managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+#   type         = "A"
+#   ttl          = 300
+#   rrdatas = [
+#     google_compute_instance.demo.network_interface[0].access_config[0].nat_ip
+#   ]
+# }
+
+resource "google_dns_record_set" "api_invy_app_com" {
+  name = "cloudrun.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  type = "CNAME"
+  ttl  = 86400
+
   managed_zone = data.google_dns_managed_zone.env_dns_zone.name
-  type         = "A"
-  ttl          = 300
-  rrdatas = [
-    google_compute_instance.demo.network_interface[0].access_config[0].nat_ip
-  ]
+
+  rrdatas = ["ghs.googlehosted.com."]
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "google_cloud_run_domain_mapping" "default" {
   location = var.region
-  name     = "my-cloudrroot7-domain-zone"
+  name     = "cloudrun.cloudroot7.xyz"
 
   metadata {
-    namespace = "my-project-name"
+    namespace = "mytesting-400910"
   }
 
   spec {
-    route_name = google_cloud_run_service.default.name
+    route_name = google_cloud_run_v2_service.cloud_run_teraform.name
   }
 }
